56f5cc146c5734f273555ba33866b7c4d9d3c205
haqueashraful <139930811+haqueashraful@users.noreply.github.com>
2023-09-14 17:20:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.m-text', {
+    strings: ['Frontend Developer.', 'Backend Developer.', 'Freelancer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  })
