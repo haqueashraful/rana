fc4c30bb36144cd967c03437f89603e937c64e15
haqueashraful <123ashrafulhaque@gmail.com>
2023-10-17 21:52:28 +0600

    changes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var typed = new Typed('.m-text', {
     strings: ['Frontend Developer.', 'Backend Developer.', 'Freelancer.'],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1000,
+    backDelay: 700,
     loop: true
   })
